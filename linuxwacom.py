4b8c84dae0e2288399e641507031821135c26115
leicas <wspeed@gmail.com>
2016-06-30 16:05:18 +0200

    Test udpconnection with wacom informations

--- linuxwacom.py
+++ linuxwacom.py
@@ -1,18 +1,28 @@
 import evdev
+import socket
+from struct import pack
 
-devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
-for device in devices:
+#SEND_ADDR = ("localhost", 5432)
+SEND_ADDR = ("134.157.19.154", 5432)
+UDPSOCK = socket.socket(type=socket.SOCK_DGRAM)
+
+DEVICES = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+for device in DEVICES:
     print(device.fn, device.name, device.phys)
-dev = evdev.InputDevice('/dev/input/event0')
-print(dev.capabilities(verbose=True))
-axis = {}
-axis['x'] = 0.0
-axis['y'] = 0.0
-axis['pressure'] = 0.0
-for event in dev.read_loop():
+DEV = evdev.InputDevice('/dev/input/event0')
+print(DEV.capabilities(verbose=True))
+AXIS = {}
+AXIS['x'] = 0.0
+AXIS['y'] = 0.0
+AXIS['pressure'] = 0.0
+for event in DEV.read_loop():
     if event.type == evdev.ecodes.EV_ABS:
-      if event.code == 0:
-       axis['x'] = event.value/100.0
-      if event.code == 1:
-       axis['y'] = event.value/100.0
-      print(str(axis['x']) + " " + str(axis['y']))
+        if event.code == 0:
+            AXIS['x'] = event.value/100.0
+        if event.code == 1:
+            AXIS['y'] = event.value/100.0
+        if event.code == 24:
+            AXIS['y'] = event.value
+        #print(str(AXIS['x']) + " " + str(AXIS['y']))
+        data = pack('dddd', 666., AXIS['x'], AXIS['y'], AXIS['pressure'])
+        UDPSOCK.sendto(data, SEND_ADDR)
---
+++ windowsudp.py
@@ -0,0 +1,25 @@
+import socket
+from struct import unpack
+
+# Creation d'un socket UDP (SOCK_DGRAM)
+
+UDPSOCK = socket.socket(type=socket.SOCK_DGRAM)
+
+# Ecoute sur port 21567 a tous les IPs
+listen_addr = ("",5432)
+#send_addr = ("localhost",5432)
+
+UDPSOCK.bind(listen_addr)
+
+
+while True:
+    # On attend un paquet de taille 1024 octets m
+    data, addr = UDPSOCK.recvfrom(1024)
+    print(len(data))
+    message = unpack('dddd', data)
+    print (message, " from \t", addr)
+  
+  #data =  pack('dddddd',666., 3.4, 4.5,7.2, 8.4, 9.5)
+
+  #print "send back now:"
+  #UDPSock.sendto(data,send_addr)
